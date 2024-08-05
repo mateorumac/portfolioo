4c53111a32422722f7aa009a224156b52a3fd67c
Mateo Rumac <85076814+mateorumac@users.noreply.github.com>
2024-08-05 16:08:44 +0200

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -28,7 +28,10 @@
           <div class="card-content">
             <p>
               A beauty salon website with working backend to process user requests.<br /> <br>
-              Sidenote: Backend takes a while (up to 1 minute) to process requests due to inactivity and free hosting.
+              Sidenote: Backend takes a while (up to 1 minute) to process requests due to inactivity and free hosting.<br>
+              Login to see more functionalities!<br>
+              E-mail: user@gmail.com <br>
+              Password: user1
             </p>
           </div>
           <div class="card-footer">
